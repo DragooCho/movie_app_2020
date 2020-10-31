4ef3748c28a78a2415d5fbe83f7edcef0a1d6775
Dragoo Cho <deagoocho@gmail.com>
2020-10-31 14:07:14 +0900

    #2 0 end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Potato from "./Potato";
+import Toto from "./Toto";
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!@#%@%!!!</h1>
+      <Potato />
+      <Toto />
     </div>
   );
 }
---
+++ src/Toto.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Toto() {
+  return <h2>I Love Toto</h2>;
+}
+
+export default Toto;
